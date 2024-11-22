a21e9b8fdaf8ef61d67673465672e70972409a8b
abduvali0320 <abduvalinormirzayev007@gmail.com>
2024-11-22 19:14:14 +0500

    icon qo'shishga kelindi muammolar ko'pligi uchun gitga qo'shb qo'yildi

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+function Home() {
+  return (
+    <View>
+        <Icon name="home" size={30} color="blue" />
+        <Text>
+            salom
+        </Text>
+    </View>
+  );
+}
+
+export default Home;
